3bb7fce43f0d8e637261029e887ee011a3f89742
C0RT3XGIT <cortexchemodan@gmail.com>
2022-09-05 17:26:16 +0300

    validation rule added

--- src/validations/movies.ts
+++ src/validations/movies.ts
@@ -2,5 +2,8 @@ import * as yup from "yup";
 
 export const movieActionSchema = yup.object().shape({
   name: yup.string().required("Email field is required!"),
-  release_year: yup.string().required("Password field is required!"),
+  release_year: yup
+    .number()
+    .typeError("Entry type should be number")
+    .required("Password field is required!"),
 });
